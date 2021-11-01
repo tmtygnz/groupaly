5ce8cfe835b1a4a513cb42b3b19acbc1470007ea
Timothy Ganoza <jostimian@gmail.com>
2021-11-01 11:42:24 +0800

    use api to get quotes

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export const Dashboard = () => {
+  const [bannerQuote, setQuote] = useState("");
+  const [bannerQuoteAuthor, setAuthor] = useState("");
+  useEffect(() => {
+    const fetchAPI = async () => {
+      let qoute = await axios.get("http://localhost:3001/quote");
+      setQuote(qoute.data.content);
+      setAuthor(qoute.data.author);
+    };
+    fetchAPI();
+  }, []);
   return (
     <div className="h-full">
       <div className="banner h-20 flex items-center justify-center bg-gradient-to-r from-green-400 to-blue-500 rounded m-3 realtive">
         <div className="m-3">
-          <span className="Quote font-bold">
-            You can't connect the dots looking forward; you can only connect
-            them looking backwards
-          </span>
-          <span className="Author font-light italic"> - Steve Jobs.</span>
+          <span className="Quote font-bold">{bannerQuote}</span>
+          <span className="Author font-light italic"> -{bannerQuoteAuthor}</span>
         </div>
       </div>
     </div>
