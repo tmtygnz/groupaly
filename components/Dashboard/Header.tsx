2eab41d519fa93431c490a952c2982f98474441a
Timothy Ganoza <jostimian@gmail.com>
2021-11-14 20:22:40 +0800

    avatar view change

--- components/Dashboard/Header.tsx
+++ components/Dashboard/Header.tsx
@@ -5,11 +5,11 @@ import { Alink } from "../ui/Alink";
 import { Avatar } from "../ui/Avatar";
 
 export const Header = () => {
-  const {user, updateUser} = useUser();
+  const { user, updateUser } = useUser();
   return (
     <div className="border-b-2 hover:shadow-xl transition duration-200 ease-in-out border-black w-screen h-14 flex items-center justify-between p-5">
       <div>
-        <InfdosLogo/>
+        <InfdosLogo />
       </div>
       <div className="flex items-center justify-center">
         <Alink href={"#"} className="mx-2">
@@ -18,9 +18,12 @@ export const Header = () => {
         <Alink href={"#"} className="mx-2">
           Settings
         </Alink>
-        <Alink href={"#"} className="mx-2">
-          <Avatar url={user?.user.photoURL!}/>
-        </Alink>
+        <div className="mx-2">
+          <div className="avatar flex items-center justify-between bg-red rounded-full p-2 hover:bg-black transition duration-200 ease-in-out">
+            <Avatar url={user?.user.photoURL!} />
+            <div className="name text-baige mx-1">{user?.user.displayName?.split(" ")[0]}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ components/Dashboard/SesssionGraph.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useUser } from "../../context/NewUserContext";
+
+export const SesssionGraph = () => {
+	const user = useUser();
+  return <div>
+
+	</div>;
+};
