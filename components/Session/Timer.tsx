f0a100462c027a414a2f9d62874fe1e10e6b22e3
Timothy Ganoza <jostimian@gmail.com>
2021-11-27 22:17:48 +0800

    Time to timer

--- components/Session/Timer.tsx
+++ components/Session/Timer.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Moment from "react-moment";
 
 export const Timer = () => {
+  const [sec, setSec] = useState(0);
+  const countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+  useEffect(() => {
+    setInterval(() => {
+      const timeNow = new Date().getTime();
+      const range = countDownDate - timeNow;
+      var seconds = Math.floor((range % (1000 * 60)) / 1000);
+      setSec(sec => seconds);
+    }, 1000);
+  }, []);
+
   return (
     <div className="bg-baige_red p-5 rounded">
-      <div className="header font-bold text-2xl">Time</div>
-      <div className="time">
-        <Moment interval={1000}>1976-04-19T12:59-0500</Moment>
-      </div>
+      <div className="header font-bold text-2xl">Timer</div>
+      <div className="time">{sec}</div>
     </div>
   );
 };
